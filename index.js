e144dc6b819b7ebc9f5d2a6df45284931cf31898
johnnyman727 <johnnyman727@gmail.com>
2014-10-14 20:27:17 -0700

    Fixed whitespace and json formatting

--- index.js
+++ index.js
@@ -4,66 +4,64 @@ var _ = require("underscore");
 
 var Celery = function(config){
     
-    var celery = {};
+  var celery = {};
 
-    celery.config = (function(config){
+  celery.config = (function(config){
 
-        if(typeof(config) === "string"){
-            var config = {
-                "key":config
-            };
-        }else if(typeof(config) == "undefined"){
-            var config = {};
-        }
-        
-        return _.defaults(config,{
-            "baseurl": 'https://api.trycelery.com',
-            "version": 1,
-        });
-
-    })(config);
-
-    celery.options = function(options){
-
-        if(typeof(options) === "string"){
-            var options = {
-                "url":options
-            };
-        }else if(typeof(options) == "undefined"){
-            var options = {};
-        }
-
-        if(dotty.exists(options,"url")){
-            options.url = (options.url.match(/^\//)) ? options.url.substring(1,options.url.length) : options.url;
-            options.url = celery.config.baseurl + "/v" + celery.config.version + "/"  + options.url + "?access_token=" + celery.config.key;
-        }
-
-        return _.defaults(options,{
-            "url": celery.config.baseurl + "/v" + celery.config.version + "/",
-            "method": "GET",
-            "headers":{
-                "Content-Type": "application/json"
-            },
-        });
-
-    };
-
-    celery.request = function(options,callback){
-        options = celery.options(options);
-        console.log('options', options)
-        request(options, function(error,response,body){
-            if(error){
-                callback(error, body);
-            }else if(response && dotty.exists(response, "statusCode")){
-                callback(false, body, response.statusCode);
-            }else{
-                callback("no response status code", body);
-            }
-        });
-        
+    if(typeof(config) === "string"){
+      var config = {
+          "key":config
+      };
+    }else if(typeof(config) == "undefined"){
+      var config = {};
     }
     
-    return celery;
+    return _.defaults(config,{
+      "baseurl": 'https://api.trycelery.com',
+      "version": 1,
+    });
+  })(config);
+
+  celery.options = function(options){
+
+    if (typeof(options) === "string"){
+      var options = {
+          "url":options
+      };
+    }
+    else if(typeof(options) == "undefined"){
+      var options = {};
+    }
+
+    if(dotty.exists(options,"url")){
+      options.url = (options.url.match(/^\//)) ? options.url.substring(1,options.url.length) : options.url;
+      options.url = celery.config.baseurl + "/v" + celery.config.version + "/"  + options.url + "?access_token=" + celery.config.key;
+    }
+
+    return _.defaults(options,{
+      "url": celery.config.baseurl + "/v" + celery.config.version + "/",
+      "method": "GET",
+      "json" : true,
+      "headers":{
+          "Content-Type": "application/json"
+      },
+    });
+  };
+
+  celery.request = function(options,callback){
+    options = celery.options(options);
+    request(options, function(error,response,body) {
+      if (error) {
+        callback(error, body);
+      } else if (response && dotty.exists(response, "statusCode")) {
+        callback(false, body, response.statusCode);
+      } else {
+        callback("no response status code", body);
+      }
+    });
+  }
+  
+  return celery;
 
 };
 
